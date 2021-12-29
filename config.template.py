53902a035e054a24c4b135124337ab38bc996d65
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2021-12-28 22:51:42 -0300

    Adding config template  file

---
+++ config.template.py
@@ -0,0 +1,8 @@
+# This is a template file, in order to use it adequately copy this file without template name and set the variables below
+# Get it from @BotFather
+BOT_TOKEN = ''
+# Get it from my.telegram.org
+API_ID = ''
+API_HASH = ''
+# Change it as you please
+DATABASE_URL = 'sqlite:///database.db'
